837356a76fd2814431ee6ae7f9a60f89810e3e6e
Gabriel Kakizaki <gkakizaki@gmail.com>
2022-11-09 21:46:32 -0300

    remove renderer class

--- src/Text.cpp
+++ src/Text.cpp
@@ -16,3 +16,8 @@ std::string Text::get_text() const {
 RGBColor Text::get_color() const {
   return _color;
 }
+
+void Text::onRender(ALLEGRO_FONT* font) {
+  al_draw_text(font, al_map_rgb(_color.red, _color.green, _color.blue),
+               _pos.get_x(), _pos.get_y(), 0, &_text.c_str()[0]);
+}
