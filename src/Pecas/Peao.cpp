3664d3295a80e5bdbc27584f779059ba278afad5
Igornorante <igor.josafa@ufv.br>
2022-11-30 12:47:37 -0300

    Adicionada funcionalidades para o mouse dentro do display

--- src/Pecas/Peao.cpp
+++ src/Pecas/Peao.cpp
@@ -2,9 +2,3 @@
 
 Peao::Peao(Cor cor, Position pos, ALLEGRO_BITMAP* sprite)
   : Peca(cor, pos, sprite) {}
-
-Position Peao::movepeca(){
-   if(ALLEGRO_EVENT_KEY_DOWN && event.mouse.x == _pos.get_x() && event.mouse.y == _pos.get_y()){
-       
-   }
-}
