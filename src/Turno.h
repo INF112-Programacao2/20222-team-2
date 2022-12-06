5df5b8d247f4a08a28f081d9ad8b0460ece8eaf4
Igornorante <igor.josafa@ufv.br>
2022-12-05 21:06:07 -0300

    Adição da classe Turno.h

---
+++ src/Turno.h
@@ -0,0 +1,21 @@
+#include <iostream> 
+#include <allegro5/allegro.h>
+#include "Tabuleiro.h"
+#include "Game.h"
+#include "Peca.h"
+
+class Turno {
+
+private:
+int _turno;
+int _turno_atual;
+
+public:
+Turno();
+~Turno();
+void setTurnoAtual(int turno_atual);
+int getTurnoAtual();
+void setTurno(int _turno);
+int getTurno();
+void trocarTurno();
+};
