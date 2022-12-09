367144d468431433501ae89d174c7ddab3b29e94
Igornorante <igor.josafa@ufv.br>
2022-12-09 00:12:25 -0300

    Restrição bispo

--- src/Pecas/Bispo.cpp
+++ src/Pecas/Bispo.cpp
@@ -10,11 +10,7 @@ bool Bispo::validarMovimento(Position pos) {
     return false;
   } else {
     //Movimentação do Bispo
-    if(_cor == Cor::PRETO  && pos.get_x() == _pos.get_x() + 1 && pos.get_y() == _pos.get_y() + 1){
-    _pos = pos;
-    return true;
-    }
-    else if(_cor == Cor::BRANCO && pos.get_x() == _pos.get_x() - 1 || pos.get_y() == _pos.get_y() - 1){
+    if(pos.get_x() == _pos.get_x() + 1 && pos.get_y() == _pos.get_y() + 1 || pos.get_x() == _pos.get_x() - 1 && pos.get_y() == _pos.get_y() - 1 || pos.get_x() == _pos.get_x() - 1 && pos.get_y() == _pos.get_y() + 1 || pos.get_x() == _pos.get_x() + 1 && pos.get_y() == _pos.get_y() - 1){
     _pos = pos;
     return true;
     }
